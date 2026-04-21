2817a2de8fbcd36eaf8acd813dd4fe0a706d38d3
Miller Sovereign <miller@sovereignhealthsystems.co.uk>
2026-04-21 13:59:34 +0100

    pitch: Chris Kearns private pitch v4

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         source: "/pitch/luke-steedman",
         destination: "/pitch/luke-steedman/index.html",
       },
+      {
+        source: "/pitch/chris-kearns",
+        destination: "/pitch/chris-kearns/index.html",
+      },
     ];
   },
   async headers() {
